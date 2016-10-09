a4288ef329a981caa5bfcbe7ecf318e7117e610c
Vinh Nguyen <viha195@gmail.com>
2016-10-09 21:50:46 +0700

    Changing the layout links

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,8 +3,12 @@ module SessionsHelper
 	def log_in(user)
 		session[:user_id] = user.id
 	end
-
+	#Return the current loged-in use (if any).
 	def current_user
 		@current_user ||= User.find_by(id: session[:user_id])
+	end
+	# Return true if the user is loged in, false otherwise.
+	def loged_in?
+		!current_user.nil?
 	end
 end
