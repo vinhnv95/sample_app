f02c589e9f1ce4e5427690b62ab78655aee49127
Vinh Nguyen <viha195@gmail.com>
2016-10-09 14:21:29 +0700

    Show user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'help'	=>	'static_pages#help'
   get 'about'	=>	'static_pages#about'
+  get 'signup'	=>	'users#new'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
