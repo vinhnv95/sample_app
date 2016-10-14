e90c6ff69473004bd610844ddad8bb3504b57a4b
Vinh Nguyen <viha195@gmail.com>
2016-10-14 18:26:50 +0700

    Showing all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_action :logged_in_user, only: [:edit, :update]
+	before_action :logged_in_user, only: [:index, :edit, :update]
 	before_action :correct_user, only: [:edit, :update]
 
 	def show
@@ -35,6 +35,10 @@ class UsersController < ApplicationController
 		else
 			render 'edit'
 		end
+	end
+
+	def index
+		@users = User.all
 	end
 	
 	private
