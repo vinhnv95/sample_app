5feff0196b8f2462c815aceefaad859abb619fd9
Vinh Nguyen <viha195@gmail.com>
2016-10-14 16:38:34 +0700

    Unsuccessful edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,18 @@ class UsersController < ApplicationController
 			render 'new'
 		end
 	end
+
 	def edit
 		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			# Handle a successful update
+		else
+			render 'edit'
+		end
 	end
 	
 	private
